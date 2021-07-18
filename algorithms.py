313a1adabecb631c550a78ca3ee953bfed1583f1
ilarionov <bombardire54@gmail.com>
2021-07-18 19:07:51 +0300

    It works but still need to be improved. Elements that equals to main element just go away

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,8 @@
 from random import randint
 
 def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
     random_number = randint(0, len(arr) - 1)
     main = arr[random_number]
     left = [elem for elem in arr if elem != main and elem < main]
@@ -8,5 +10,5 @@ def quick_sort(arr):
     return quick_sort(left) + [main] + quick_sort(right)
 
 
-unsorted = [3, 6, 23, 12, 2, 6, 87]
-quick_sort(unsorted)
+unsorted = [3, 3, 6, 6, 23, 12, 2, 6, 87]
+print(quick_sort(unsorted))
