51b3909e97ddf58a4d93de87c37a2cacb98b7fb1
ilarionov <bombardire54@gmail.com>
2021-07-18 22:17:26 +0300

    Create min element sorting

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,7 @@
 from random import randint
 
 # it is a quick function sort
-# @ params: unsorted (probably) array
+# @ params: unsorted array
 # @ returns sorted array (if works)
 def quick_sort(arr):
     # exit way
@@ -18,6 +18,20 @@ def quick_sort(arr):
     return quick_sort(left) + [main] + quick_sort(right)
 
 
+# it is a sorting by min element
+# @ params: unsorted array
+# @ returns sorted array (if works)
+def min_element_sort(arr):
+    sorted = []
+
+    while len(arr):
+        min_element = min(arr)
+        sorted.append(min_element)
+        arr.remove(min_element)
+
+    return sorted
+
+
 # declare an unsorted array
 unsorted = [3, 3, 6, 6, 23, 12, 2, 6, 11, 87]
 print(quick_sort(unsorted))
@@ -25,3 +39,12 @@ print(quick_sort(unsorted))
 # test with strings
 unsorted_names = ['Vlad', 'Bob', 'Nikita', 'Lesha', 'Rodik']
 print(quick_sort(unsorted_names))
+
+
+# declare an unsorted array
+unsort = [3, 3, 6, 6, 23, 12, 2, 6, 11, 87]
+print(min_element_sort(unsort))
+
+# test with strings
+unsort_names = ['Vlad', 'Bob', 'Nikita', 'Lesha', 'Rodik']
+print(min_element_sort(unsort_names))
