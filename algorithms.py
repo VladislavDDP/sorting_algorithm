55683198e4b16ca07ea7f1c425bab233cbe32a55
ilarionov <bombardire54@gmail.com>
2021-07-18 19:14:14 +0300

    Fixed problem with loosing element that equals to main

--- algorithms.py
+++ algorithms.py
@@ -1,14 +1,23 @@
 from random import randint
 
+# it is a quick function sort
+# @ params: unsorted (probably) array
+# @ returns sorted array (if works)
 def quick_sort(arr):
+    # exit way
     if len(arr) < 2:
         return arr
+    # basic way
     random_number = randint(0, len(arr) - 1)
     main = arr[random_number]
-    left = [elem for elem in arr if elem != main and elem < main]
-    right = [elem for elem in arr if elem != main and elem > main]
+    arr.remove(main)
+
+    # fill left and right arrays with elements
+    left = [elem for elem in arr if elem < main]
+    right = [elem for elem in arr if elem >= main]
     return quick_sort(left) + [main] + quick_sort(right)
 
 
-unsorted = [3, 3, 6, 6, 23, 12, 2, 6, 87]
+# declare an unsorted array
+unsorted = [3, 3, 6, 6, 23, 12, 2, 6, 11, 87]
 print(quick_sort(unsorted))
